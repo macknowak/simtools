f94d5b2d6e3dfa4d4deb92132f2f90780beb6a78
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2017-06-08 19:28:02 +0200

    Add package initializer

---
+++ simtools/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""SimTools: Python tools for simulation management.
+
+SimTools provides a set of tools facilitating management of simulations in
+Python.
+"""
+
+__version__ = '0.1.0.dev1'
+__author__ = "Przemyslaw (Mack) Nowak"
