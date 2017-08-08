d363b8b6a2706bce940dcc0eae39fad15103b765
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2017-08-08 19:48:14 +0200

    Add generating simulation directory name
    Generating simulation directory name is added as a simulation launch
    service. By default, simulation directory name is generated based on the
    simulation id and possibly prefixed with an underscore if the simulation
    directory is considered temporary.

--- simtools/simrun.py
+++ simtools/simrun.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 """Simulation launch services.
 
-Simulation launch services provide generating simulation id based on local date
-and time.
+Simulation launch services provide the following functionality:
+
+- generating simulation id based on local date and time;
+- generating simulation directory name.
 """
 
 import time
+
+TMP_DIR_PREFIX = "_"
 
 
 def generate_sim_id():
@@ -14,3 +18,10 @@ def generate_sim_id():
     sim_id = "{0:04}{1:02}{2:02}_{3:02}{4:02}{5:02}".format(
         t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
     return sim_id
+
+
+def generate_sim_dirname(tmp=False, sim_id=None):
+    """Generate simulation directory name."""
+    if not sim_id:
+        sim_id = generate_sim_id()
+    return sim_id if not tmp else TMP_DIR_PREFIX + sim_id
--- simtools/tests/test_simrun.py
+++ simtools/tests/test_simrun.py
@@ -3,12 +3,35 @@
 
 import time
 
-from simtools.simrun import generate_sim_id
+import pytest
+
+from simtools.simrun import generate_sim_id, generate_sim_dirname
 
 
-def test_generate_sim_id(monkeypatch):
+@pytest.fixture
+def local_time(monkeypatch):
     t = time.strptime("2000-10-30 07:08:09", "%Y-%m-%d %H:%M:%S")
     monkeypatch.setattr(time, 'localtime', lambda: t)
 
+
+def test_generate_sim_id(local_time):
     sim_id = generate_sim_id()
     assert sim_id == "20001030_070809"
+
+
+def test_generate_sim_dirname(local_time):
+    # Default
+    sim_dirname = generate_sim_dirname()
+    assert sim_dirname == "20001030_070809"
+
+    # Temporary directory, no simulation id
+    sim_dirname = generate_sim_dirname(tmp=True)
+    assert sim_dirname == "_20001030_070809"
+
+    # Simulation id
+    sim_dirname = generate_sim_dirname(sim_id="20001030_070809")
+    assert sim_dirname == "20001030_070809"
+
+    # Temporary directory, simulation id
+    sim_dirname = generate_sim_dirname(tmp=True, sim_id="20001030_070809")
+    assert sim_dirname == "_20001030_070809"
