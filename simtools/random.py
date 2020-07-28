8e9b8450a77963a408f5b51cabaf5abb50a5ac09
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2020-07-28 16:52:30 +0200

    Add argument for generating random seed
    An optional argument for generating random seed using OS-specific
    randomness source is added, which allows specifying whether the
    generated random seed should not exceed the upper limit of the signed or
    the unsigned integer represented in the same number of bytes.

--- simtools/random.py
+++ simtools/random.py
@@ -11,7 +11,7 @@ import sys
 MAX_N_BYTES = 256
 
 
-def generate_seed(n_bytes=None):
+def generate_seed(n_bytes=None, unsigned_limit=False):
     """Generate random seed using OS-specific randomness source."""
     # If the number of bytes is specified, validate it, otherwise determine it
     # based on the largest positive integer supported by the platform
@@ -27,10 +27,12 @@ def generate_seed(n_bytes=None):
     # Generate random bytes using OS-specific randomness source
     rand_bytes = os.urandom(n_bytes).encode('hex')
 
-    # If the highest bit is 1, change it to 0 (to avoid using Python long
-    # integers)
-    highest_nibble_val = int(rand_bytes[0], 16)
-    if highest_nibble_val > 7:
-        rand_bytes = str(highest_nibble_val - 8) + rand_bytes[1:]
+    # If the generated random seed should not exceed the upper limit of the
+    # signed integer represented in the same number of bytes and the highest
+    # bit in the generated random bytes is 1, change it to 0
+    if not unsigned_limit:
+        highest_nibble_val = int(rand_bytes[0], 16)
+        if highest_nibble_val > 7:
+            rand_bytes = str(highest_nibble_val - 8) + rand_bytes[1:]
 
     return int(rand_bytes, 16)
--- simtools/tests/test_random.py
+++ simtools/tests/test_random.py
@@ -39,8 +39,18 @@ def test_generate_seed_n_bytes_out_range(n_bytes):
         gen_seed = generate_seed(n_bytes)
 
 
-def test_generate_seed_highest_bit(monkeypatch):
+@pytest.mark.parametrize('maxsize, signed_seed, unsigned_seed', [
+    (2**31 - 1, 8421504, 2155905152),
+    (2**63 - 1, 36170086419038336, 9259542123273814144)])
+def test_generate_seed_unsigned_limit(monkeypatch, maxsize, signed_seed,
+                                      unsigned_seed):
     monkeypatch.setattr(os, 'urandom', lambda n: "\x80" * n)
+    monkeypatch.setattr(sys, 'maxsize', maxsize)
 
-    gen_seed = generate_seed(4)
-    assert gen_seed == 8421504
+    # Unsigned integer limit
+    gen_seed = generate_seed(unsigned_limit=True)
+    assert gen_seed == unsigned_seed
+
+    # Signed integer limit
+    gen_seed = generate_seed(unsigned_limit=False)
+    assert gen_seed == signed_seed
